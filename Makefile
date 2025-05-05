dc1d5b5590c6dd5f46431aaf3ae25912c44daceb
yvictor <yvictor3141@gmail.com>
2025-05-05 17:03:42 +0800

    feat: use camoufox to solve turnstile

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 image:
 	docker build -t capsolver-api .
 
+image-ext:
+	docker build -t capsolver-api -f Dockerfile.ext .
+
 run:
 	docker run --restart always --name capsolver-api --env-file ./.env -d -p 9987:9987 capsolver-api
 
--- src/capsolver_api/app.py
+++ src/capsolver_api/app.py
@@ -3,10 +3,14 @@ import os
 import capsolver
 import requests
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from .utils import get_turnstile_token
 
 app = FastAPI()
+
+
 capsolver.api_key = os.getenv("CAPSOLVER_API_KEY")
+TOKENS_QUEUE = []
 
 @app.get("/")
 def read_root():
@@ -41,6 +45,26 @@ def turnstile_capsolver(url: str, website_key: str):
         }
     )
     return {"token": solution["token"]}
+
+@app.get("/turnstile/solver")
+async def turnstile_solver(url: str, website_key: str, headless: bool = True, browser_type: str = "camoufox"):
+    res = await get_turnstile_token(url, website_key, headless=headless, browser_type=browser_type)
+    if res.status == "success":
+        return {"token": res.turnstile_value}
+    else:
+        raise HTTPException(status_code=500, detail=res.reason)
+
+@app.get("/turnstile/collect")
+def turnstile_collect(token: str):
+    TOKENS_QUEUE.append(token)
+    return {"message": "Token collected"}
+
+
+@app.get("/turnstile/get")
+def turnstile_get():
+    if len(TOKENS_QUEUE) == 0:
+        return {"error": "No tokens in queue"}
+    return {"token": TOKENS_QUEUE.pop(0)}
 
 
 def main():
